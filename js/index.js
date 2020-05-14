5eb3e597b07b4409396bbe337c1d72e2aa3221ee
kt-five <katie5five.5@gmail.com>
2020-05-14 22:51:24 +0200

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -13,8 +13,8 @@ $(window).ready(function(){
     $(".header").css("background-size", "cover")
 
     }else if (width >600){
-        $(".bg-video").add("#video")
-        $(".bg-video").show()
+        $(".bg-video").add(".bg-video__content")
+   
     }
 })
 
@@ -22,15 +22,15 @@ $(window).on("resize", function(){
     width = $(window).width();
     if( width < 600 ){
 
-     $(".bg-video").remove("#video")
+     $(".bg-video").remove(".bg-video__content")
      $(".bg-video").hide()
     $(".header").css("background-color", "transparent")
     $(".header").css("background-image", "linear-gradient(rgba(89, 241, 255, 0.39), rgba(89, 241, 255, 0.39)), url(img/sansilvestrehead.jpg)")
     $(".header").css("background-size", "cover")
 
     }else if (width >600){
-        $(".bg-video").add("#video")
-        $(".bg-video").show()
+        $(".bg-video").add(".bg-video__content")
+        
         $(".header").css({"background-color": ""})
         $(".header").css({"background-image": ""})
         $(".header").css({"background-size": ""})
