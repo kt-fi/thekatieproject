7a914575e8644d99f298b4305f802aa31f670426
kt-five <katie5five.5@gmail.com>
2020-07-31 05:18:11 +0200

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -2,4 +2,7 @@ let width = window.innerWidth;
 
 if(width < 500){
         document.querySelector('.aboutMe__container').setAttribute("data-aos", "fade-in-pink")
+        document.querySelector('.aboutMe__container').setAttribute("data-aos-duration", "500")
+        document.querySelector(".section__cv").setAttribute("data-aos-duration", "500")
+        document.querySelector(".section__cv").setAttribute("data-aos", "fade-in-blue")
 }
