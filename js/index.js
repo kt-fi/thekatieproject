d7e01f3ae3ffd6ace47f1633675926e1ee1ebca0
kt-five <katie5five.5@gmail.com>
2020-07-31 16:46:21 +0200

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -7,8 +7,16 @@ if(width < 500){
         document.querySelector(".section__cv").setAttribute("data-aos", "fade-in-blue")
 }
 
+
+
 document.querySelector(".slideNav__close").addEventListener("click", function(){
         document.querySelector(".slideNav__drawer").setAttribute("style", "transform: translateX(100%)")
         document.querySelector(".slideNav").setAttribute("style",     "background-color: rgba(0, 0, 0, 0)")
         // document.querySelector(".slideNav").setAttribute("style", "z-index: -1;")
+})
+
+document.querySelector(".slideNav__drawer--tag").addEventListener("click", function () {
+        document.querySelector(".slideNav__drawer").setAttribute("style", "transform: translateX(0)")
+        document.querySelector(".slideNav").setAttribute("style",     " background-color: rgba(0, 0, 0, 0.411)")
+       ;
 })
