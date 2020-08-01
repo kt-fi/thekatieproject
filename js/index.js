670e566f0c6cef574083915a0af49d848870e5b3
kt-five <katie5five.5@gmail.com>
2020-08-01 20:40:01 +0200

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,71 +1,54 @@
 let width = window.innerWidth;
 
-if(width > 500){
-        
+if (width > 500) {
+
 }
 
-$(window).scroll(function(){
+$(window).scroll(function () {
 
-        if(scrollY > 50){
-if(width > 500){
-        $(".header__logo--small").css("margin-top", "-2rem" )      
-}else if(width < 500){
-        $(".header__logo--small").css("top", "-1.5rem" )    
-        $(".header__logo--small").css("padding-left", "0rem" )
-        
-}
-          
+        if (scrollY > 50) {
+                if (width > 500) {
+                        $(".header__logo--small").css("margin-top", "-2rem")
+                } else if (width < 500) {
+                        $(".header__logo--small").css("top", "-2rem")
+                        $(".header__logo--small").css("padding-left", "0rem")
+                }
+
+                        $(".header__logo--small").css("scale", "0.7")
+                        $(".slideNav__drawer--tag").css("top", "3rem")
+                        $(".nav").css("height", "11rem")
+                        $(".header__links").css("top", "1.5rem")
+                        $(".header__links").css("padding-bottom", "1rem")
+                if (width > 500) {
+                        $(".header__logo--small").css("padding-left", "4rem")
+                } else if (width < 500) {
+                        $(".header__logo--small").css("padding-left", "1rem")
+                }
 
 
-        $(".header__logo--small").css("scale", "0.8" )
-        
 
-        $(".slideNav__drawer--tag").css("top", "3rem" )
+                }
+                if (scrollY < 50) {
+                        $(".header__logo--small").css("scale", "1")
+                        $(".header__logo--small").css("top", "2rem")
+                        $(".slideNav__drawer--tag").css("top", "4rem")
+                        $(".nav").css("height", "13rem")
+                        $(".header__links").css("top", "4rem")
+                }
 
-         $(".nav").css("height", "10rem" )
 
-         $(".header__links").css("top", "1.5rem" ) 
-         $(".header__links").css("padding-bottom", "1rem" )     
-if(width > 500){
-        $(".header__logo--small").css("padding-left", "4rem" )
 
-}else if (width < 500){
-          $(".header__logo--small").css("padding-left", "1rem" )
-}
-       
-         
-    
-        }if(scrollY < 50){
-                $(".header__logo--small").css("scale", "1" )
-                $(".header__logo--small").css("top", "2rem" )
-                $(".slideNav__drawer--tag").css("top", "5rem" )
 
-                $(".nav").css("height", "13rem" )
-
-                $(".header__links").css("top", "4rem" )   
-        }
-            
-        
-    
-     
-    })
-    
-    
-    
-
+})
 
 function cssFunction() {
         document.querySelector("body").classList.remove("preload");
         console.log('hello')
-      };
-
-
-     
-     
+};
 
 
 if (width < 500) {
-        document.querySelector(".header__logo--small").setAttribute("style", "padding-left:1rem" )
+        document.querySelector(".header__logo--small").setAttribute("style", "padding-left:1rem")
         document.querySelector('.aboutMe__container').setAttribute("data-aos", "fade-in-pink")
         document.querySelector('.aboutMe__container').setAttribute("data-aos-duration", "1000")
         document.querySelector(".section__cv").setAttribute("data-aos-duration", "1000")
@@ -75,7 +58,7 @@ if (width < 500) {
 function close() {
         document.querySelector(".slideNav__drawer").setAttribute("style", "transform: translateX(106%)")
         document.querySelector(".slideNav").setAttribute("style", " visibility: hidden")
-      
+
 }
 
 function open() {
@@ -100,6 +83,4 @@ document.querySelector(".slideNav__drawer--tag").addEventListener("click", funct
 document.querySelector(".slideNav").addEventListener("click", function () {
         close();
 })
-
-
 
