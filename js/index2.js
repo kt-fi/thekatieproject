e2af3827fc0de93863f68049c7c5b25ecb4869ee
kt-five <katie5five.5@gmail.com>
2020-11-17 00:16:06 +0100

    Delete index2.js

--- js/index2.js
+++
@@ -1,7 +0,0 @@
-
-let top = document.querySelector(window).offsetTop();
-
-if(top > 10){
-        document.querySelector(".header__logo--small").setAttribute("style", "width: 90%");
-        alert("hello")
-}
